8d9040e231f80c38278093203b5cf64ea7e09aa0
AskMHX <askmhx@gmail.com>
2021-02-28 22:35:36 +0800

    add language template

--- src/ddl.rs
+++ src/ddl.rs
@@ -1,28 +1,57 @@
 use std::io::{BufReader, BufRead, Write};
 use std::fs::File;
-use crate::{CONST_REGEX_START, CONST_REGEX_ROW, CONST_REGEX_END};
+use std::collections::HashMap;
+use regex::Regex;
 
 pub fn gen_model(lang: &str, in_file: File, mut out_file: File) {
+
+    let lang_go: &str = "GO";
+    let lang_java: &str = "JAVA";
+    let lang_rust: &str = "RUST";
+
+    let mut template_start: HashMap<&str, &str> = HashMap::new();
+    let mut template_end: HashMap<&str, &str> = HashMap::new();
+    let mut template_row: HashMap<&str, &str> = HashMap::new();
+
+    let regex_start: Regex = Regex::new(r"CREATE\s+TABLE\s+\S+.(?P<title>\S+$)").unwrap();
+    let regex_end: Regex = Regex::new(r"\)\s+CHARSET\s+=\s+UTF8MB4(\s+)?;(\s+)?").unwrap();
+    let regex_row: Regex = Regex::new(r"^\S{4}(\S+)\s+(\S+),?$").unwrap();
+
+
+    template_start.insert(lang_go, "struct %s{");
+    template_start.insert(lang_java, "public class %s{");
+    template_start.insert(lang_rust, "pub struct %s {");
+
+    template_row.insert(lang_go, "%s %s");
+    template_row.insert(lang_java, "%s %s;");
+    template_row.insert(lang_rust, "%s:%s,");
+
+
+    template_end.insert(lang_go, "}");
+    template_end.insert(lang_java, "}");
+    template_end.insert(lang_rust, "}");
+
+
     let buffered: BufReader<File> = BufReader::new(in_file);
 
     for line in buffered.lines().map(|x| x.unwrap()) {
         println!("{}", line);
 
-        if CONST_REGEX_START.is_match(line.as_str()) {
+        if regex_start.is_match(line.as_str()) {
             println!("match title:{}", line);
-            let title = &(CONST_REGEX_START.captures(&line).unwrap())["title"];
+            let title = &(regex_start.captures(&line).unwrap())["title"];
             out_file.write_all(title.as_bytes());
             out_file.write_all(b"\n");
         }
 
-        if CONST_REGEX_ROW.is_match(line.as_str()) {
+        if regex_row.is_match(line.as_str()) {
             println!("match colum:{}", line);
             //out_file.write_all( titleRegex.captures_iter(&line)[0].at(0).unwrap_or(""))
         }
 
-        if CONST_REGEX_END.is_match(line.as_str()) {
+        if regex_end.is_match(line.as_str()) {
             println!("match end:{}", line);
-            let end = CONST_REGEX_END.captures(&line).unwrap().get(0).unwrap().as_str();
+            let end = regex_end.captures(&line).unwrap().get(0).unwrap().as_str();
             out_file.write_all(end.as_bytes());
             out_file.write_all(b"\n");
         }
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,9 @@
 
 mod ddl;
 
-use std::io::{BufReader, BufRead, Write};
 use std::fs::File;
-use regex::Regex;
-use std::collections::HashMap;
-
-const CONST_LANG_GO: &str = "GO";
-const CONST_LANG_JAVA: &str = "JAVA";
-const CONST_LANG_RUST: &str = "RUST";
-
-const CONST_TEMPLATE_STAT: HashMap<&str, &str> = HashMap::new();
-const CONST_TEMPLATE_END: HashMap<&str, &str> = HashMap::new();
-const CONST_TEMPLATE_ROW: HashMap<&str, &str> = HashMap::new();
-
-const CONST_REGEX_START: Regex = Regex::new(r"CREATE\s+TABLE\s+\S+.(?P<title>\S+$)").unwrap();
-const CONST_REGEX_END: Regex = Regex::new(r"\)\s+CHARSET\s+=\s+UTF8MB4(\s+)?;(\s+)?").unwrap();
-const CONST_REGEX_ROW: Regex = Regex::new(r"^\S{4}(\S+)\s+(\S+),?$").unwrap();
-
 
 fn main() {
-
-    CONST_TEMPLATE_STAT.insert(CONST_LANG_GO, "struct %s{");
-    CONST_TEMPLATE_STAT.insert(CONST_LANG_JAVA, "public class %s{");
-    CONST_TEMPLATE_STAT.insert(CONST_LANG_RUST, "pub struct %s {");
-
-    CONST_TEMPLATE_ROW.insert(CONST_LANG_GO, "%s %s");
-    CONST_TEMPLATE_ROW.insert(CONST_LANG_JAVA, "%s %s;");
-    CONST_TEMPLATE_ROW.insert(CONST_LANG_RUST, "%s:%s,");
-
-
-    CONST_TEMPLATE_END.insert(CONST_LANG_GO, "}");
-    CONST_TEMPLATE_END.insert(CONST_LANG_JAVA, "}");
-    CONST_TEMPLATE_END.insert(CONST_LANG_RUST, "}");
-
 
     let lang = std::env::args().nth(1).expect("Missing input file path").to_uppercase();
 
