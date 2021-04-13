d6bc3d5be78d38c228d4dfd78d5b73644f9de616
AskMHX <askmhx@gmail.com>
2021-04-13 18:11:11 +0800

    There are a lot of mistakes, but don't worry, I'll deal with them

--- src/database.rs
+++ src/database.rs
@@ -1,43 +1,49 @@
-use std::fs::File;
-// use crate::{CONST_REGEX_ROW, CONST_REGEX_END, CONST_REGEX_START};
 use mysql::Pool;
 use mysql::prelude::Queryable;
+use std::borrow::Borrow;
 
 pub struct Table {
-     pub name :String,
-     pub fields:Ver<Field>,
+    pub name: String,
+    pub fields: Ver<Field>,
 }
 
 pub struct Field {
-   pub fname:String,
-   pub ftype:String,
-   pub fcollection:String,
-   pub fnull:String,
-   pub fkey:String,
-   pub fdefault:String,
-   pub fextra:String,
-   pub fprivilages:String,
-   pub fcomment:String,
+    pub fname: String,
+    pub ftype: String,
+    pub fcollection: String,
+    pub fnull: String,
+    pub fkey: String,
+    pub fdefault: String,
+    pub fextra: String,
+    pub fprivilages: String,
+    pub fcomment: String,
 }
 
 pub fn gen_model_from_database(db_url: &str) -> Vec<Table> {
-
-    let tables:Vec<Table> = Vec::new();
+    let tables: Vec<Table> = Vec::new();
 
     let pool = Pool::new(db_url).unwrap();
 
     let mut conn = pool.get_conn().unwrap();
 
-    let result: Vec<String> = conn.query("show tables").unwrap();
+    let result: Vec<String> = conn.query("SHOW TABLES").unwrap();
 
-    for table in result {
-        let sql_show_table = format!("SHOW FULL COLUMNS FROM {};", table);
-        println!("{}", table.as_str());
+    for table_name in result {
+        let mut table = Table();
+        table.name = table_name;
+
+        let mut fields: Vec<Field> = Vec::new();
+
+        let sql_show_table = format!("SHOW FULL COLUMNS FROM {};", table_name);
+
         let mut table_conn = pool.get_conn().unwrap();
         table_conn.query_map(sql_show_table, |(field, ftype, fcollection, fnull, fkey, fdefault, fextra, fprivilages, fcomment)| {
-            println!("{},{},{},{},{},{},{},{},{}", field, ftype, fcollection, fnull, fkey, fdefault, fextra, fprivilages, fcomment);
-        }).unwrap()
+            fields.push(Field(field, ftype, fcollection, fnull, fkey, fdefault, fextra, fprivilages, fcomment))
+        }).unwrap();
+        table.fields = fields;
+
+        tables.borrow().push(table);
     }
 
-    return tables
+    return tables;
 }
